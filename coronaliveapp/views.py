690e018055eeb507e8ec1a4703c2f6a9857f18f8
Adarsh Biradar <adarsh.s.biradar@gmail.com>
2020-03-24 08:34:48 +0530

    added total cases

--- coronaliveapp/views.py
+++ coronaliveapp/views.py
@@ -39,11 +39,16 @@ def home(req):
     data['total']=data['indian']+data['foreign']+data['discharged']+data['death']
     data.sort_values(by='total',axis=0,ascending=False,inplace=True)
     statewise = data.to_dict()
+    info=dict()
     for ke in statewise:
         statewise[ke]=list(statewise[ke].values())
-
+    counts=soup.findAll("span", {"class": "icount"})
+    label=soup.findAll("div", {"class": "info_label"})
+    info_labels=['passengers','total','cured','death','migrated']
+    info_counts=[remove_html_tags(x) for x in counts]
+    info=dict(zip(info_labels,info_counts))
     print(json.dumps(statewise))
-    return render(req,'home.html',{'data': mark_safe(json.dumps(statewise))})
+    return render(req,'home.html',{'data': mark_safe(json.dumps(statewise)),'info':mark_safe(json.dumps(info)),'info2':json.dumps(info)})
     #return JsonResponse(data)
     #return HttpResponse(response.text)
 
@@ -95,15 +100,11 @@ def adarsh_home(req):
     statewise = data.to_dict()
     for ke in statewise:
         statewise[ke]=list(statewise[ke].values())
-    """
     counts=soup.findAll("span", {"class": "icount"})
     label=soup.findAll("div", {"class": "info_label"})
-    info=dict()
-    for i in range(len(counts)):
-        info[remove_html_tags(label[i])]=[0]
-    for i in range(len(counts)):
-        info[remove_html_tags(label[i])].append(get_count(remove_html_tags(counts[i])))
-    """
-    return render(req,'home.html',{'data': mark_safe(json.dumps(statewise))})
+    info_labels=['passengers','total','cured','death','migrated']
+    info_counts=[remove_html_tags(x) for x in counts]
+    info=dict(zip(info_labels,info_counts))
+    return render(req,'home.html',{'data': mark_safe(json.dumps(statewise)),'info':mark_safe(json.dumps(info),),'info2':json.dumps(info)})
     #return JsonResponse(data)
     #return HttpResponse(response.text)
